912cbd03f90d071993312c08074183de329cff90
Pascal Knoth <pascal@malach.it>
2025-03-29 14:24:37 +0100

    formatting

--- lib/rpn.ex
+++ lib/rpn.ex
@@ -1,14 +1,17 @@
 defmodule Rpn do
   def compute(input) do
     with nil <- Regex.run(~r/^-?\d+$/, input),
-      [_all, operation, a, b, operator, _end] <- Regex.run(~r/((-?\d+) (-?\d+) (\-|\+|\*|\/))([^\d]|$)/, input),
-         res <- apply(Kernel, String.to_atom(operator), [String.to_integer(a), String.to_integer(b)]) do
+         [_all, operation, a, b, operator, _end] <-
+           Regex.run(~r/((-?\d+) (-?\d+) (\-|\+|\*|\/))([^\d]|$)/, input),
+         res <-
+           apply(Kernel, String.to_atom(operator), [String.to_integer(a), String.to_integer(b)]) do
       input
       |> String.replace(operation, to_string(round(res)))
       |> compute()
     else
       [result] ->
         {:ok, String.to_integer(result)}
+
       _ ->
         {:error, "Invalid input."}
     end
