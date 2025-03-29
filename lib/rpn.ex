10ac308f36d8cad20ce984bf53a9f589db01442d
Pascal Knoth <pascal@malach.it>
2025-03-29 14:15:07 +0100

    replace eval_string to apply

--- lib/rpn.ex
+++ lib/rpn.ex
@@ -2,7 +2,7 @@ defmodule Rpn do
   def compute(input) do
     with nil <- Regex.run(~r/^-?\d+$/, input),
       [_all, operation, a, b, operator, _end] <- Regex.run(~r/((-?\d+) (-?\d+) (\-|\+|\*|\/))([^\d]|$)/, input),
-      {res, _bindings} <- Code.eval_string("#{a} #{operator} #{b}") do
+         res <- apply(Kernel, String.to_atom(operator), [String.to_integer(a), String.to_integer(b)]) do
       input
       |> String.replace(operation, to_string(round(res)))
       |> compute()
